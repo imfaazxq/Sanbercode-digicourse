6615e3eb310479d6126db9d2ea24ade6a9777874
imfaazxq <khafifahramadhan@gmail.com>
2025-02-22 20:30:51 +0800

    tugas 15 CRUD

--- newsApp/routes/web.php
+++ newsApp/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\BiodataController;
+use App\Http\Controllers\CastsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,19 @@ Route::get('/data-table', function(){
 Route::get('/table', function(){
     return view ('pages.table');
 });
+
+// CRUD CASTS
+// CREATE DATA
+Route::get('Cast/create', [CastsController::class, "create"]);
+Route::post('/Cast',[CastsController::class, "store"]);
+
+// READ DATA
+Route::get('/Cast',[CastsController::class, "index"]);
+Route::get('/Cast/{casts_id}',[CastsController::class, "show"]);
+
+// UPDATE DATA
+Route::get('/Cast/{casts_id}/edit',[CastsController::class, "edit"]);
+Route::put('/Cast/{casts_id}',[CastsController::class, "update"]);
+
+// DELETE DATA
+Route::delete('/Cast/{casts_id}',[CastsController::class, "destroy"]);
